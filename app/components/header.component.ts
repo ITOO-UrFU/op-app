2a75ae8f487cc27315829a08a6dc674a9278a70f
leaalex <macpro@mac-pro-mac.at.urfu.ru>
2017-01-30 09:48:02 +0500

    поменял местами кнопи

--- app/components/header.component.ts
+++ app/components/header.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit  } from '@angular/core';
 @Component({
   selector: 'header-comp',
   template: `<div class="header-comp">
-  <a routerLink="/courses">Курсы</a>
   <a routerLink="/main">Главная</a>
+  <a routerLink="/courses">Курсы</a>
   <a routerLink="/register">Регистрация</a>
   </div><router-outlet></router-outlet>
   `,
